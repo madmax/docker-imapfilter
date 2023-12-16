b0e7a9608073066be4914f7b334227a9f5dcd03e
Grzegorz Derebecki <grzegorz.derebecki@fdb.pl>
2023-12-16 02:09:40 +0100

    upgrade lua from 5.1 to 5.4

--- Dockerfile
+++ Dockerfile
@@ -6,12 +6,12 @@ ARG imapfilter_spec=master
 # Original from simbelmas:
 # https://github.com/simbelmas/dockerfiles/tree/master/imapfilter
 
-RUN apk --no-cache add lua openssl pcre git \
-    && apk --no-cache add -t dev_tools lua-dev openssl-dev make gcc libc-dev pcre-dev pcre2-dev \
-    && git clone https://github.com/lefcha/imapfilter.git /imapfilter_build \
-    && cd /imapfilter_build \
-    && git checkout "${imapfilter_spec}" \
-    && make && make install
+RUN apk --no-cache add lua5.4 openssl pcre git \
+  && apk --no-cache add -t dev_tools lua5.4-dev openssl-dev make gcc libc-dev pcre-dev pcre2-dev \
+  && git clone https://github.com/lefcha/imapfilter.git /imapfilter_build \
+  && cd /imapfilter_build \
+  && git checkout "${imapfilter_spec}" \
+  && C_INCLUDE_PATH=/usr/include/lua5.4/ LIBRARY_PATH=/usr/lib/lua5.4/ make && make install
 
 FROM alpine
 
@@ -20,10 +20,10 @@ COPY --from=builder /usr/local/share/imapfilter /usr/local/share/imapfilter
 COPY --from=builder /usr/local/man /usr/local/man
 
 COPY entrypoint.sh /entrypoint.sh
-RUN chmod a+x /entrypoint.sh && apk --no-cache add lua lua-dev openssl pcre git \
-    && adduser -D -u 1001 imapfilter \
-    && mkdir /opt/imapfilter \
-    && chown imapfilter: /opt/imapfilter
+RUN chmod a+x /entrypoint.sh && apk --no-cache add lua5.4 lua5.4-dev openssl pcre git \
+  && adduser -D -u 1001 imapfilter \
+  && mkdir /opt/imapfilter \
+  && chown imapfilter: /opt/imapfilter
 
 USER imapfilter
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env sh
 
 config_in_vcs() {
-    [ -n "$GIT_USER" ] && [ -n "$GIT_TOKEN" ] && [ -n "$GIT_TARGET" ]
+	[ -n "$GIT_USER" ] && [ -n "$GIT_TOKEN" ] && [ -n "$GIT_TARGET" ]
 }
 
 config_target_prefix="/opt/imapfilter/config"
 if config_in_vcs; then
-    config_target="$config_target_prefix/$IMAPFILTER_CONFIG"
+	config_target="$config_target_prefix/$IMAPFILTER_CONFIG"
 else
-    config_target="$IMAPFILTER_CONFIG"
+	config_target="$IMAPFILTER_CONFIG"
 fi
 
 log_parameter=
@@ -18,70 +18,70 @@ fi
 
 updated_config=no
 pull_config() {
-    updated_config=no
-    if [ ! -d "$config_target_prefix" ]; then
-        printf ">>> Config has not been cloned yet, cloning\n"
-        mkdir -p "$config_target_prefix"
-        updated_config=yes
-        git clone "https://$GIT_USER:$(cat $GIT_TOKEN)@$GIT_TARGET" "$config_target_prefix"
-    else
-        cd "$config_target_prefix"
-        printf ">>> Pulling config\n"
-        git remote update
-        if [ "$(git rev-parse HEAD)" != "$(git rev-parse FETCH_HEAD)" ]; then
-            updated_config=yes
-            git pull
-        fi
-        cd -
-    fi
+	updated_config=no
+	if [ ! -d "$config_target_prefix" ]; then
+		printf ">>> Config has not been cloned yet, cloning\n"
+		mkdir -p "$config_target_prefix"
+		updated_config=yes
+		git clone "https://$GIT_USER:$(cat $GIT_TOKEN)@$GIT_TARGET" "$config_target_prefix"
+	else
+		cd "$config_target_prefix"
+		printf ">>> Pulling config\n"
+		git remote update
+		if [ "$(git rev-parse HEAD)" != "$(git rev-parse FETCH_HEAD)" ]; then
+			updated_config=yes
+			git pull
+		fi
+		cd -
+	fi
 }
 
 if config_in_vcs; then
-    pull_config
-    cd "$config_target_prefix"
+	pull_config
+	cd "$config_target_prefix"
 elif [ -n "$IMAPFILTER_CONFIG_BASE" ]; then
-    cd "$IMAPFILTER_CONFIG_BASE"
+	cd "$IMAPFILTER_CONFIG_BASE"
 else
-    cd "${config_target%/*}"
+	cd "${config_target%/*}"
 fi
 
 if [ ! -f "$config_target" ]; then
-    printf "Config file '%s' does not exist\n" "$config_target"
-    exit 1
+	printf "Config file '%s' does not exist\n" "$config_target"
+	exit 1
 fi
 
 imapfilter_pid=
 imapfilter_update() {
-    if [ -n "$imapfilter_pid" ]; then
-        kill -TERM "$imapfilter_pid"
-        wait "$imapfilter_pid"
-    fi
-    imapfilter -c "$config_target" $log_parameter &
-    imapfilter_pid="$(jobs -p)"
+	if [ -n "$imapfilter_pid" ]; then
+		kill -TERM "$imapfilter_pid"
+		wait "$imapfilter_pid"
+	fi
+	imapfilter -c "$config_target" $log_parameter &
+	imapfilter_pid="$(jobs -p)"
 }
 
 if [ "$IMAPFILTER_DAEMON" = "yes" ]; then
-    imapfilter_update
+	imapfilter_update
 fi
 
 while true; do
-    if config_in_vcs; then
-        printf ">>> Updating config\n"
-        if ! pull_config; then
-            printf ">>> Pulling config failed\n"
-        fi
-    fi
+	if config_in_vcs; then
+		printf ">>> Updating config\n"
+		if ! pull_config; then
+			printf ">>> Pulling config failed\n"
+		fi
+	fi
 
-    if [ "$IMAPFILTER_DAEMON" = "yes" ]; then
-        if [ "$updated_config" = "yes" ]; then
-            printf ">>> Restarting imapfilter daemon\n"
-            imapfilter_update
-        fi
-    else
-        printf ">>> Running imapfilter\n"
-        imapfilter -c "$config_target" $log_parameter
-    fi
+	if [ "$IMAPFILTER_DAEMON" = "yes" ]; then
+		if [ "$updated_config" = "yes" ]; then
+			printf ">>> Restarting imapfilter daemon\n"
+			imapfilter_update
+		fi
+	else
+		printf ">>> Running imapfilter\n"
+		imapfilter -c "$config_target" $log_parameter
+	fi
 
-    printf ">>> Sleeping\n"
-    sleep "${IMAPFILTER_SLEEP:-30}"
+	printf ">>> Sleeping\n"
+	sleep "${IMAPFILTER_SLEEP:-30}"
 done
